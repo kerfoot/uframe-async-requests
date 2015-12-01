e449815555834c9506263c7113898c1c6bb5decf
John Kerfoot <johnkerfoot@gmail.com>
2015-11-30 21:14:06 -0500

    check .nc destination remotely

--- check_async_requests_from_csv.py
+++ check_async_requests_from_csv.py
@@ -19,13 +19,31 @@ def main(args):
         
     for r in csv_reader:
         
+        if r[0].startswith('#'):
+            continue
+            
         request_meta = {cols[i]:r[i] for i in col_range}
         
+        if not request_meta['outputURL']:
+            sys.stderr.write('No destination url specified: {:s}\n'.format(request_meta['request_url']))
+            sys.stderr.flush()
+            continue
+            
         if 'completion_time' not in request_meta.keys():
             request_meta['completion_time'] = None
         
         if not request_meta['completion_time']:
-            completion_time = check_async_request_availability(request_meta)
+            request_url = request_meta['outputURL']
+            
+            # Currently, we can only validate agains hyrax, not tds.
+            if not args.tds:
+                request_url = re.sub('8090/thredds/catalog/ooi/_nouser',
+                    '8080/opendap/hyrax/async_results/_nouser',
+                    request_meta['outputURL'])
+                
+            # Replace the url endpoint to point to the status.txt
+            request_url = re.sub('catalog.html$', 'status.txt', request_url)
+            completion_time = check_async_request_availability(request_url)
             request_meta['completion_time'] = completion_time   
         else:
             sys.stderr.write('Request already completed: {:s}\n'.format(request_meta['outputURL']))
@@ -48,6 +66,10 @@ if __name__ == '__main__':
     arg_parser = argparse.ArgumentParser(description=main.__doc__)
     arg_parser.add_argument('request_csv',
             help='CSV filename containing queued UFrame request.')
+    arg_parser.add_argument('--tds',
+        help='Validate agains THREDDS, not hyrax (default)\n',
+        dest='tds',
+        action='store_true')
 
     parsed_args = arg_parser.parse_args()
 
